28eb997c122fad6ffd6529b665f3d04d67007ceb
Cindy <cindy.ol@hotmail.fr>
2021-04-13 00:44:16 +0200

    modif back for credentials

--- src/main/java/com/ecolepratique/rapport/securite/WebSecurityConfig.java
+++ src/main/java/com/ecolepratique/rapport/securite/WebSecurityConfig.java
@@ -1,12 +1,8 @@
 package com.ecolepratique.rapport.securite;
 
-import java.time.Duration;
 import java.util.Arrays;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 //@EnableWebSecurity
@@ -50,13 +44,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	// Permettre aprés modification de modifier des données 
         http.cors().and().csrf().disable();
         
+        http.cors().configurationSource(request -> {
+            CorsConfiguration cors = new CorsConfiguration();
+            cors.setAllowedOriginPatterns(Arrays.asList("*"));
+            cors.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(Arrays.asList("*"));
+            cors.setAllowCredentials(true);
+            return cors;
+        });
+        
         // Toutes les requêtes envoyées a serveur web doivent être authentifiées
         http.authorizeRequests().anyRequest().authenticated();
  
         // Use AuthenticationEntryPoint to authenticate user/password
         http.httpBasic().authenticationEntryPoint(authEntryPoint);        
         
-    }
+        }
     
 //    @Bean
 //    CorsConfigurationSource corsConfigurationSource() {
